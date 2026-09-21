90f3d9e21bf2986589e5d89bdb09be605b4a84bb
Shivamani <shivamani@users.noreply.github.com>
2026-09-21 07:12:56 +0000

    feat: add terraform outputs

---
+++ outputs.tf
@@ -0,0 +1,24 @@
+output "vpc_id" {
+  description = "ID of the VPC."
+  value       = aws_vpc.this.id
+}
+
+output "public_subnet_id" {
+  description = "ID of the public subnet."
+  value       = aws_subnet.public.id
+}
+
+output "internet_gateway_id" {
+  description = "ID of the Internet Gateway."
+  value       = aws_internet_gateway.this.id
+}
+
+output "public_route_table_id" {
+  description = "ID of the public route table."
+  value       = aws_route_table.public.id
+}
+
+output "security_group_id" {
+  description = "ID of the security group allowing SSH and HTTP."
+  value       = aws_security_group.web.id
+}
